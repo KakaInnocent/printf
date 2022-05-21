d8509376a38e9ae9f5c2cd7d4a5268dbd6322856
Mambo Innocent <saneugene847@gmail.com>
2022-05-21 06:52:33 -0700

    second commit

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ *_putchar - writes the character to stdout
+ *@c: character to print
+ *Return: 0 success, -1 error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

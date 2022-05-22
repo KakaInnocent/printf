90e65f9d6ddae51f89ea12ca4d857b0101dd1a22
KamauRuth <rkamau573@gmail.com>
2022-05-22 20:25:29 +0300

    printf

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is basically a printf clone.
+
+#copyright.
+Copyright (c) 2021 Ruth Kamau & Innocent Mambo
--- _printf.c
+++ _printf.c
@@ -1,50 +1,49 @@
 #include "main.h"
 
 /**
- *_printf - produces an output according to a format specified
- *@format: conatins the charachters and specifiers
- *Return: length of output string
+ *_printf - Print a formatted string
+ *@format: format string
+ *Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	int (*function)(va_list, char *, unsigned int);
-	va_list l;
-	char *buffer;
+	int count = 0;
+	va_list list;
+	char *pointer, *start;
+	param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-	va_start(l, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	va_start(list, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (pointer = (char *)format; *pointer; pointer++)
 	{
-		if (format[i] == '%')
+		init_params(&flags, list);
+		if (*pointer != '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(l);
-				return (-1);
-			}
-			else
-			{	function = get_print(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handle_buffer(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(l, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			count += _putchar(*pointer);
+			continue;
 		}
+		start = pointer;
+		pointer++;
+		while (get_flags(pointer, &flags))
+		{
+			pointer++;
+		}
+		pointer = get_width(pointer, &flags, list);
+		pointer = get_precision(pointer, &flags, list);
+		if (get_mods(pointer, &flags))
+			pointer++;
+
+		if (!func_parse(pointer))
+			count += print_range(start, pointer,
+			flags.l_mod || flags.h_mod ? pointer - 1 : 0);
 		else
-			handle_buffer(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+			count += print_func(pointer, list, &flags);
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(l);
-	return (len);
+	_putchar(-1);
+	va_end(list);
+	return (count);
 }
